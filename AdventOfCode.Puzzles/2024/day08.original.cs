9eb5833a88276a791f4c7b25e5240a578a67d0f3
= <videogamefpslover@gmail.com>
2024-12-09 15:12:35 -0500

    Day 8 part 2 complete

--- AdventOfCode.Puzzles/2024/day08.original.cs
+++ AdventOfCode.Puzzles/2024/day08.original.cs
@@ -12,7 +12,9 @@ public class day08_original : IPuzzle
             allAntennas.Where(antenna2 => antenna2.Item1.Equals(antenna)).Select(antenna2 => antenna2.Item2).ToArray()).ToArray();
         var antinodes = antennaGroupings.Select(antennas => FindAntinodes(antennas, input.Lines.Length)).ToArray();
         var part1 = antinodes.Flatten().Distinct().Count().ToString();
-        var part2 = string.Empty;
+        var allAntinodes = antennaGroupings.Select(antennas => FindAllAntinodes(antennas, input.Lines.Length))
+            .ToArray();
+        var part2 = allAntinodes.Flatten().Distinct().Count().ToString();
         return (part1, part2);
     }
 
@@ -37,5 +39,26 @@ public class day08_original : IPuzzle
             select (2 * comparison.Item1 - reference.Item1, 2 * comparison.Item2 - reference.Item2))
             .Where(antinode => antinode.Item1 < mapSize && antinode.Item2 < mapSize && 
                                antinode.Item1 >= 0 && antinode.Item2 >= 0).ToArray();
+    }
+
+    private static (int, int)[] FindAllAntinodes((int, int)[] antennas, int mapSize)
+    {
+        var antinodes = new List<(int, int)>();
+        foreach (var refer in antennas)
+        {
+            foreach (var compare in antennas)
+            {
+                if (refer.Equals(compare)) continue;
+                var slope = (compare.Item1 - refer.Item1, compare.Item2 - refer.Item2);
+                var antinode = (refer.Item1 + slope.Item1, refer.Item2 + slope.Item2);
+                while (antinode.Item1 < mapSize && antinode.Item2 < mapSize &&
+                       antinode.Item1 >= 0 && antinode.Item2 >= 0)
+                {
+                    antinodes.Add(antinode);
+                    antinode = (antinode.Item1 + slope.Item1, antinode.Item2 + slope.Item2);
+                }
+            }
+        }
+        return antinodes.Distinct().ToArray();
     }
 }
