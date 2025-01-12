be96ba8a38c7600fa2e42cc89f2da2607f2a02a5
Kate Luinenburg <videogamefpslover@gmail.com>
2025-01-11 19:01:18 -0500

    zoomin!

--- AdventOfCode.Puzzles/2024/day11.original.cs
+++ AdventOfCode.Puzzles/2024/day11.original.cs
@@ -1,13 +1,12 @@
-using System.Collections.Concurrent;
-
 namespace AdventOfCode.Puzzles._2024;
 
 [Puzzle(2024, 11, CodeType.Original)]
 public class Day11Original : IPuzzle
 {
+	private static readonly Dictionary<(uint, uint), ulong> Memoization = new ();
 	public (string, string) Solve(PuzzleInput input)
 	{
-		var stones = input.Text.Trim().Split().Select(long.Parse).ToArray();
+		var stones = input.Text.Trim().Split().Select(uint.Parse).ToArray();
 		var stones25 = Blink(stones, 25);
 		var stones75 = Blink(stones, 75);
 		
@@ -18,17 +17,22 @@ public class Day11Original : IPuzzle
 		return (part1, part2);
 	}
 
-	private static long Blink(long[] stones, int maxIterations)
+	private static ulong Blink(uint[] stones, uint maxIterations)
 	{
-		return stones.Aggregate(0L, (total, current) => BlinkHelper(current, 0, maxIterations));
+		Memoization.Clear();
+		return stones.Aggregate(0uL, (total, current) => total + BlinkHelper(current, 0, maxIterations));
 	}
 
-	private static long BlinkHelper(long stone, int iteration, int maxIterations)
+	private static ulong BlinkHelper(uint stone, uint iteration, uint maxIterations)
 	{
-		if (iteration == maxIterations) return 1;
+		if (iteration >= maxIterations) return 1;
 
-		var num = 0L;
-		if (stone == 0)
+		var num = 0uL;
+		if (Memoization.ContainsKey((stone, iteration)))
+		{
+			num += Memoization[(stone, iteration)];
+		}
+		else if (stone == 0)
 		{
 			num += BlinkHelper(1, iteration+1, maxIterations);
 		}
@@ -39,9 +43,11 @@ public class Day11Original : IPuzzle
 		else
 		{
 			var stoneLength = stone.ToString().Length;
-			num += BlinkHelper((long) (stone / Math.Pow(10, stoneLength/2)), iteration+1, maxIterations);
-			num += BlinkHelper((long) (stone % Math.Pow(10, stoneLength/2)), iteration+1, maxIterations);
+			num += BlinkHelper((uint) (stone / Math.Pow(10, stoneLength/2)), iteration+1, maxIterations);
+			num += BlinkHelper((uint) (stone % Math.Pow(10, stoneLength/2)), iteration+1, maxIterations);
 		}
+
+		Memoization.TryAdd((stone, iteration), num);
 
 		return num;
 	}
